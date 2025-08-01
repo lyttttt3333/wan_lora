07d80179c611996599177a84e6162d21d5e52af7
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-01 11:58:00 +0800

    Update utils.py

--- diffsynth/trainers/utils.py
+++ diffsynth/trainers/utils.py
@@ -377,13 +377,16 @@ class ModelLogger:
     
     
     def on_epoch_end(self, accelerator, model, epoch_id, steps):
-        state_dict = accelerator.get_state_dict(model)
-        state_dict = accelerator.unwrap_model(model).export_trainable_state_dict(state_dict, remove_prefix=self.remove_prefix_in_ckpt)
-        state_dict = self.state_dict_converter(state_dict)
-        os.makedirs(self.output_path, exist_ok=True)
-        path = os.path.join(self.output_path, f"epoch-{epoch_id}-{steps}.safetensors")
-        print("wait3")
-        accelerator.save(state_dict, path, safe_serialization=True)
+        unwrapped_model = accelerator.unwrap_model(model)
+        path = os.path.join(self.output_path, f"epoch-{epoch_id}-{steps}")
+        unwrapped_model.save_pretrained(path, save_function=accelerator.save)
+        # state_dict = accelerator.get_state_dict(model)
+        # state_dict = accelerator.unwrap_model(model).export_trainable_state_dict(state_dict, remove_prefix=self.remove_prefix_in_ckpt)
+        # state_dict = self.state_dict_converter(state_dict)
+        # os.makedirs(self.output_path, exist_ok=True)
+        # path = os.path.join(self.output_path, f"epoch-{epoch_id}-{steps}.safetensors")
+        # print("wait3")
+        # accelerator.save(state_dict, path, safe_serialization=True)
             # checkpoint = {
             #     'epoch': epoch_id,
             #     'step': steps,
