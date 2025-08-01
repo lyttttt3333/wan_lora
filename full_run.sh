67a059a37e9d106be17be8fd6001cf559e48b924
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-01 23:25:21 +0800

    Update full_run.sh

--- full_run.sh
+++ full_run.sh
@@ -1,3 +1,6 @@
+source /opt/dlami/nvme/caoyuan/miniconda/bin/activate
+conda activate lora
+
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_API_KEY="5409d3b960b01b25cec0f6abb5361b4022f0cc41"
 
@@ -10,7 +13,7 @@ accelerate launch examples/wanvideo/model_training/train.py \
   --dataset_repeat 100 \
   --model_id_with_origin_paths "Wan-AI/Wan2.2-TI2V-5B:diffusion_pytorch_model*.safetensors,Wan-AI/Wan2.2-TI2V-5B:models_t5_umt5-xxl-enc-bf16.pth,Wan-AI/Wan2.2-TI2V-5B:Wan2.2_VAE.pth" \
   --learning_rate 1e-5 \
-  --num_epochs 2 \
+  --num_epochs 3 \
   --remove_prefix_in_ckpt "pipe.dit." \
   --output_path "./models/train/Wan2.2-TI2V-5B_full" \
   --trainable_models "dit" \
