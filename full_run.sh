d39dde250cbac12b7c314b6a3ef3250a22163074
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-01 22:40:43 +0800

    Update full_run.sh

--- full_run.sh
+++ full_run.sh
@@ -1,3 +1,4 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_API_KEY="5409d3b960b01b25cec0f6abb5361b4022f0cc41"
 
 accelerate launch examples/wanvideo/model_training/train.py \
@@ -13,4 +14,5 @@ accelerate launch examples/wanvideo/model_training/train.py \
   --remove_prefix_in_ckpt "pipe.dit." \
   --output_path "./models/train/Wan2.2-TI2V-5B_full" \
   --trainable_models "dit" \
-  --extra_inputs "input_image"
+  --extra_inputs "input_image" \
+  --lora False
